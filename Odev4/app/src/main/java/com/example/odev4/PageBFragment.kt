856cb1dd962e1e6a81d783697f358460e2bd86ce
Anil Yasar <anlysr2002@gmail.com>
2025-05-07 14:48:49 +0300

    Odev4 Fragment A, B and navigation from A to B completed

--- Odev4/app/src/main/java/com/example/odev4/PageBFragment.kt
+++ Odev4/app/src/main/java/com/example/odev4/PageBFragment.kt
@@ -6,23 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
-import com.example.odev4.databinding.FragmentPageABinding
+import com.example.odev4.databinding.FragmentMainPageBinding
+import com.example.odev4.databinding.FragmentPageBBinding
 
-class PageAFragment : Fragment() {
+class PageBFragment : Fragment() {
 
-    private lateinit var binding: FragmentPageABinding
-
+    private lateinit var binding: FragmentPageBBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentPageABinding.inflate(inflater, container, false)
+        binding = FragmentPageBBinding.inflate(inflater, container, false)
 
-        binding.buttonToPageB.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.gecisB)
-        }
+//        binding.buttonToPageB.setOnClickListener {
+//            Navigation.findNavController(it).navigate(R.id.gecisB)
+//        }
+
         return binding.root
     }
-
 }
